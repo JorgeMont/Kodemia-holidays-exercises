c76e6d970427ed3413f10cd50bfab67bb09cd42a
George <monterrosaspix@gmail.com>
2023-01-08 01:49:52 -0600

    Cleaning code

--- ejercicio1.js
+++ ejercicio1.js
@@ -2,7 +2,6 @@
 //     Solicitar el nombre completo de un usuario e indicarle cuantos caracteres tiene su nombre
 
 const getNameLength = (name) => {
-    let getNameLength = 0;
     name = name.replaceAll(' ', '');
     return `Tu nombre tiene ${name.length} caracteres`;
 }
